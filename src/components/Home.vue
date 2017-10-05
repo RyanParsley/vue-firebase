62a0a935b7b5e1448033cf65a707c980ed9e296a
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2017-10-04 21:26:35 -0400

    stub out basic routes and auth

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout column>
+    <v-flex xs12 class="text-xs-center" mt-5>
+      <h3>Home</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDryBvko1inBiog8cx0LJvWoLU7yPEtbjk',
+  authDomain: 'vue-firebase-95229.firebaseapp.com',
+  databaseURL: 'https://vue-firebase-95229.firebaseio.com',
+  projectId: 'vue-firebase-95229',
+  storageBucket: 'vue-firebase-95229.appspot.com',
+  messagingSenderId: '651637362130'
+}
